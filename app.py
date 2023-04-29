250dc9dc4cd7528873f45c09aa4905a649afe74f
dark7ngihts <alalimohammed258@gmail.com>
2023-04-29 06:23:40 +0300

    embryonic form of flask app

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-from flask import Flask
+from flask import Flask, redirect
+
+# configure application
+app = Flask(__name__)
+
+# set home page
+@app.route('/')
+def index():
+    return f'Hello and welcome to our humble site'
+
+# set users page
+@app.route('/<name>')
+def user(name):
+    return f'Hello, {name}'
+
+# set admin page
+@app.route('/admin')
+def admin():
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
