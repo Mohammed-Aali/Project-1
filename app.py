fb5768a0aae19995dfb5bdd60b12ed0520a46e19
dark7ngihts <alalimohammed258@gmail.com>
2023-05-01 06:10:09 +0300

    deleted most files, left the image because I"ll use it later

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+from cs50 import SQL
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+
+
+# set up our database
+db = SQL('sqlite:///users.db')
 
 # configure application
 app = Flask(__name__)
@@ -22,18 +27,30 @@ def login():
         session.permanant = True
         user = request.form['username']
         session['user'] = user
+        flash('Login is successful!', 'info')
         return redirect(url_for("user"))
     else:
         if 'user' in session:
             return redirect(url_for('user'))
         return render_template('login.html')
 
-@app.route('/user')
+@app.route('/user', methods=['POST', 'GET'])
 def user():
+    email = None
     if 'user' in session:
         user = session['user']
-        return f'<h1>{user}</h1>'
+
+        if request.method == 'POST':
+            email = request.form.get('email')
+            session['email'] = email
+            flash('email is submitted', 'info')
+        else:
+            if 'email' in session:
+                email = session['email']
+    
+        return render_template('user.html', email=email)
     else:
+        flash('You are not logged in', 'info')
         return redirect(url_for('login'))
 
 @app.route('/logout')
@@ -41,6 +58,7 @@ def logout():
     if 'user' in session:
         flash(f'{session.get("user")} has logged out successfully', 'info')
         session.pop('user', None)
+        session.pop('email', None)
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
