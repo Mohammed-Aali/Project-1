e6b98968ec5ef8bd0a5182829281a913838fdf1e
dark7ngihts <alalimohammed258@gmail.com>
2023-05-02 09:47:53 +0300

    done with footer for now, design comes later

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from cs50 import SQL
+from flask import Flask, render_template, flash, redirect, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
+# configure application
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# configure CS50 Library to use SQLite database
+db = SQL('sqlite:///portfolio.db')
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route('/')
 def index():
-    return 'hello world'
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
