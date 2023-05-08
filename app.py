b87957f6d03d3e3b2d849ff4a095582be41cba4d
dark7ngihts <alalimohammed258@gmail.com>
2023-05-08 11:23:00 +0300

    started with integration of the email confirmation way, considering adding a forget password, and a resend code messages

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import re, os
+import re
+
 from cs50 import SQL
 from flask import Flask, render_template, flash, redirect, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology
+from helpers import apology, generate_code, register_required, send_email
 
 # configure application
 app = Flask(__name__)
@@ -26,6 +27,7 @@ def after_request(response):
     return response
 
 @app.route('/')
+@app.route('/home/')
 def index():
     """Display Projects"""
     return render_template('index.html')
@@ -50,7 +52,7 @@ def register():
         email = request.form.get('email')
         password = request.form.get('password')
         confirm = request.form.get('confirmation')
-
+        
         # set up regexp for checks
         password_regexp = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,32}$"
         email_regex = r"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$"
@@ -59,21 +61,42 @@ def register():
         check_email_regexp = re.search(email_regex, email)
         check_password_regexp = re.search(password_regexp, password)
 
-        # go to confirm page for code
+        # check for code confirmation and
         if not check_password_regexp:
-            return apology('Always has been', 'Invalid Password')
+            return apology('Invalid Password', 'Valid Password')
         elif not check_email_regexp:
-            return apology("Always has been",'Invalid Email')
+            return apology("Invalid Email",'Valid Email')
+        elif str(confirm) != str(password):
+            return apology('invalid Confirmation','Valid Confirmation')
         else:
+            session['email'] = email
+            session['code'] = generate_code(6)
+            send_email(email, session.get('code'))
+
             return redirect('/confirm')
     else:
-        print('bye')
         return render_template('register.html')
 
 @app.route('/confirm', methods=['GET', 'POST'])
+@register_required
 def confirm():
     if request.method == 'POST':
-        return apology('confirmation is a success', 404)
+        # set up the variabes
+        confirm_code = request.form.get('confirmationCode')
+        email = session.get('email', None)
+        code = session.get('code', None)
+        msg = "<div class='invalid-feedback pt-1 d-block' role='alert'> Please enter a valid confirmation code. </div>"
+        
+        # incase we could not send the code
+        if code == None or email == None:
+            return apology('Confirmation Code sent normally', 'Erorr in sending could not send normally')
+
+        # compare confirm code againts random code
+        if not (code == confirm_code):
+            return render_template('confirm.html', msg=msg)
+
+        # if all went well and no checks were triggered
+        return redirect('/')
     else:
         return render_template("confirm.html")
 
@@ -84,8 +107,8 @@ def logout():
     # forget any user_id
     session.clear()
 
-    # redirect user to login form
-    return redirect('/login')
+    # redirect user to front page
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
--- helpers.py
+++ helpers.py
@@ -1,4 +1,8 @@
-from flask import render_template
+import random, string, secrets, os, smtplib
+
+from email.message import EmailMessage
+from flask import render_template, session, redirect
+from functools import wraps
 
 # this code snippet is from cs50x final project finance
 def apology(message, code=400):
@@ -14,3 +18,70 @@ def apology(message, code=400):
         return s
 
     return render_template("apology.html", top=code, bottom=escape(message)), code
+
+# this code snippet is also from cs50x final project finance
+def register_required(f):
+    """
+    Decorate routes to require registration.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("email") is None:
+            return redirect("/register")
+        return f(*args, **kwargs)
+    return decorated_function
+
+# this code generates a string of chars and digits that is six chars long
+def generate_code(length):
+    code = ''.join(secrets.choice(string.ascii_lowercase + string.digits) for x in range(length))
+    return str(code) 
+
+# this code sends an email to the users email for confimation 
+def send_email(receiver, code):
+
+    # define sender, receiver, subject and message body
+    sender = os.environ.get('EMAIL_ADDRESS')
+    password = os.environ.get('EMAIL_PASSWORD')
+    subject = 'Your registration confirmation'
+    message_body = f"""<html>
+    <h3>Hello, and thank you for signing up with us</h3>
+
+    Please copy the code below and paste it into your browser to confirm your email address, and activate your account.
+    
+
+    <div style="text-align:center">
+
+        <p>👇👇👇Your confirmation code is 👇👇👇:</p>
+                    
+                    <h1>{code}</h1>
+    </div>
+
+    
+    <p> If you did not sign up with us, please ignore this email. This may have been a mistake or a fraudulent attempt to use your email address. </p>
+
+    <p>Best regards, 3ffect team 👻.</p>
+    </html>"""
+
+    # create an EmailMessage object
+    msg = EmailMessage()
+    msg['Subject'] = 'Registration Confirmation'
+    msg['From'] = sender
+    msg['To'] = receiver
+
+    # set the msg body as html
+    msg.set_content(message_body, subtype='html')
+
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(sender, password)
+            smtp.send_message(msg)
+            print("Successfully sent email")
+    except socket.error as e:
+        return apology(f"Could not connect to server - is it down? ({e.errno}): {e.strerror}")
+
+    except smtplib.SMTPResponseException as e:
+        error_code = e.smtp_code
+        error_message = e.smtp_error
+        return apology(f"Server returned an error code: {error_code}", error_message)
